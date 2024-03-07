34dab1224d3d6e45ee6640c5ac49234a722aa761
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-03-07 22:58:42 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+from rembg import remove
+
+input_path = input('Your File Name: ')
+output_path = 'output.png'
+
+with open(input_path, 'rb') as i:
+    with open(output_path, 'wb') as o:
+        input = i.read()
+        output = remove(input)
+        o.write(output)
